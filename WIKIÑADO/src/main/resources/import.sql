9af9846ed7f7c92f25d24792210ce166a04c9d35
Alex Victoria <Alexander250113@gmail.com>
2023-08-21 22:58:08 +0200

    precargo propuesta de artículo a angel para que el supervisor tenga solo una en la lista

--- WIKIÑADO/src/main/resources/import.sql
+++ WIKIÑADO/src/main/resources/import.sql
@@ -57,3 +57,9 @@ INSERT INTO `authorities` (user_id, authority) VALUES (3,'ROLE_COORDINADOR');
 INSERT INTO `authorities` (user_id, authority) VALUES (3,'ROLE_COLABORADOR');
 
 INSERT INTO peticiones_rol (id, requested_authority, status, user_id) VALUES (1, 'ROLE_COORDINADOR', 'PENDIENTE', 1 )
+
+INSERT INTO `propuestas` (estado, propuesta, articulo_id, usuario_id) VALUES ('EVALUACION','Esto es una prueba para cargar la propuesta desde el import sql', 1, 1 );
+INSERT INTO `propuestas` (estado, propuesta, articulo_id, usuario_id) VALUES ('EVALUACION','Este es la única propuesta pendiente de revisión que le debería de salir al Supervisor', 2, 1 );
+INSERT INTO `propuestas` (estado, propuesta, articulo_id, usuario_id) VALUES ('EVALUACION','Esto es una prueba para cargar la propuesta desde el import sql', 3, 1 );
+INSERT INTO `propuestas` (estado, propuesta, articulo_id, usuario_id) VALUES ('EVALUACION','Esto es una prueba para cargar la propuesta desde el import sql', 4, 1 );
+
